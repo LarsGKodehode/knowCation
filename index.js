bf9e306d55f52dff6d23e5000938a60429dc69e7
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-04 16:08:24 +0200

    Put all location refrences to one spot
    hardcoded Stavanger as temporary location to use for testing purposes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import weatherWidget from "./components/weather-widget.js";
 
 
 const weatherBergen = weatherWidget({
-  "location": "Bergen",
-  "coordinates": {"latitude": 60.389444, "longditude": 5.33},
-})
+  "location": "Stavanger",
+  "coordinates": {"latitude": 58.963333, "longditude": 5.718889},
+});
