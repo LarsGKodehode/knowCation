0f9f161ea8c6804b2b284c31bf8c34803d0cdaef
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-04 16:04:50 +0200

    weather widget work
    still need to decide what we want to display of forecast, now? tommorow? there's 82 hours of forecast from yr.no. Might look into historic data, from other suppliers

--- components/weather-widget.js
+++ components/weather-widget.js
@@ -1,14 +1,41 @@
 const weatherWidget = (options) => {
-  
-  nodeRaw = `
+  /**
+   * Header to send with external API request
+   */
+   const header = new Headers({
+    "User-Agent"   : navigator.userAgent
+  });
+
+  const nodeRaw = `
   <div>
 
 
   </div>
-  `
+  `;
 
-  const domNode = new DOMParser().parseFromString(nodeRaw).body.firstChild;
+  const domNode = new DOMParser().parseFromString(nodeRaw, "text/html").body.firstChild;
 
+  /**
+   * 
+   * @param {*} latitude {float}
+   * @param {*} longditude {float}
+   * @returns 
+   */
+  async function getWeather(latitude, longditude) {
+    if(latitude === undefined || longditude === undefined) {
+      console.warn(`Can't find weather for unspecified place`);
+      return;
+    }
+    const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longditude}`;
+  
+    const dataRaw = await fetch(url, header);
+    const dataParsed = await dataRaw.json();
+    console.log(dataParsed);
+  };
+
+  getWeather(options.coordinates.latitude, options.coordinates.longditude);
+
+  // returned handles
   return {
     domNode,
   };
--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 import weatherWidget from "./components/weather-widget.js";
 
 
-async function catchWeather() {
-  const url = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json";
-  
-  const newDataRaw = await fetch(url);
-  const newDataParsed = await newDataRaw.json()
-
-  console.log(newDataParsed);
-};
-
-async function catchImage() {
-  const url = "https://api.unsplash.com/";
-
-  const dataRaw = await fetch(url);
-  console.log(dataRaw);
-  const dataParsed = await dataRaw.json();
-  
-  console.log(dataParsed);
-};
-
-
-//catchWeather();
-// catchImage();
+const weatherBergen = weatherWidget({
+  "location": "Bergen",
+  "coordinates": {"latitude": 60.389444, "longditude": 5.33},
+})
