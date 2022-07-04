13282803b8b1aa1e79447b1786a09703c9e2b75d
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-04 18:07:20 +0200

    weather widget fetching and parsing working

--- components/weather-widget.js
+++ components/weather-widget.js
@@ -1,43 +1,89 @@
-const weatherWidget = (options) => {
-  /**
-   * Header to send with external API request
-   */
-   const header = new Headers({
-    "User-Agent"   : navigator.userAgent
-  });
+const weatherWidget = (createInfo) => {
+  // ===== Public Members =====
+  let weatherForecast = []; // location to store gathered weather data
 
-  const nodeRaw = `
-  <div>
-
-
-  </div>
-  `;
-
-  const domNode = new DOMParser().parseFromString(nodeRaw, "text/html").body.firstChild;
-
-  /**
-   * 
-   * @param {*} latitude {float}
-   * @param {*} longditude {float}
-   * @returns 
-   */
-  async function getWeather(latitude, longditude) {
-    if(latitude === undefined || longditude === undefined) {
-      console.warn(`Can't find weather for unspecified place`);
-      return;
-    }
-    const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longditude}`;
   
-    const dataRaw = await fetch(url, header);
-    const dataParsed = await dataRaw.json();
-    console.log(dataParsed);
+  // ===== Public Methods =====
+  /**
+   * Updates internal weather forecast
+   */
+  async function updateForecast() {
+    weatherForecast = await getWeatherForecast();
   };
 
-  getWeather(options.coordinates.latitude, options.coordinates.longditude);
+  /**
+   * @returns internaly stored weatherforecast
+   */
+  function getForecast() {
+    return weatherForecast;
+  };
+
+
+  // ===== Private Members =====
+  const location = createInfo.location;
+  const coordinates = createInfo.coordinates;
+  requiredFields(); // extracted guard clauses into own function
+
+
+  // ===== Private Methods =====
+  // checks if all createInfo required fields are present
+  function requiredFields(createInfo) {
+    if(location === undefined) {
+      console.warn(`Need a name for location`)
+      return false;
+    };
+    if(coordinates.latitude === undefined || coordinates.longitude === undefined) {
+      console.warn(`Can't find weather for unspecified place.`);
+      return false;
+    };
+    if(typeof(coordinates.latitude) !== "number" || typeof(coordinates.longitude) !== "number") {
+      console.warn(`Can only find location with latitude(float) and longitude(float).`);
+      return false;
+    };
+    return true;
+  };
+  
+  // gets new weather data
+  async function getWeatherForecast() {
+    // place to store retrived data
+    let newForecast = [];
+
+    // API endpoint
+    const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+    // Header to send with external API request
+    const header = new Headers({
+      "User-Agent": navigator.userAgent,
+      "cache": "force-cache",
+    });
+  
+    // fetch and parse
+    const dataRaw = await fetch(url, header);
+    const dataParsed = await dataRaw.json();
+    
+    // unpack data
+    const weatherSeries = dataParsed.properties.timeseries;
+    weatherSeries.forEach((element) => {
+      const timeStamp = element.time;
+      const temperature = element.data.instant.details.air_temperature
+      const cloudCoverage = element.data.instant.details.cloud_area_fraction;
+
+      // store gathered data
+      newForecast.push({
+        "time": timeStamp,
+        "temperature": temperature,
+        "cloudCoverage": cloudCoverage,
+      });
+    });
+
+    return newForecast;
+  };
+  
+
 
   // returned handles
   return {
-    domNode,
+    getForecast,
+    updateForecast,
   };
 };
 
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import weatherWidget from "./components/weather-widget.js";
 
 
-const weatherBergen = weatherWidget({
+const weatherOutput = weatherWidget({
   "location": "Stavanger",
-  "coordinates": {"latitude": 58.963333, "longditude": 5.718889},
+  "coordinates": {"latitude": 58.963333, "longitude": 5.718889},
+});
+
+console.log(weatherWidget.weatherForecast);
+
+const buttonRefreshWeather = document.getElementById("refresh-weather");
+buttonRefreshWeather.addEventListener("click", (e) => {
+  weatherOutput.updateForecast();
+});
+
+const buttonDevLog = document.getElementById("dev-log");
+buttonDevLog.addEventListener("click", () => {
+  console.log(weatherOutput.getForecast());
 });
